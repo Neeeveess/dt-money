280e168469e1fd6cbde3cdca0ac3a3bd0c5358c5
João Vitor Neves <jvneves_marq@hotmail.com>
2023-12-20 17:51:54 -0300

    dt-money aula de http

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,26 @@
+import * as Dialog from '@radix-ui/react-dialog'
+
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+
+
+
+import logoImg from '../../assets/images/logo.svg'
+import { NewTransactionModal } from '../NewTransactionModal';
+
+export function Header(){
+  return (
+  <HeaderContainer>
+    <HeaderContent>
+      <img src={logoImg} alt="" />
+
+      <Dialog.Root>
+        <Dialog.Trigger asChild>
+          <NewTransactionButton>Novo Lançamento</NewTransactionButton>
+        </Dialog.Trigger>
+        <NewTransactionModal/>
+      </Dialog.Root>
+
+    </HeaderContent>
+  </HeaderContainer>
+  ) 
+ }
